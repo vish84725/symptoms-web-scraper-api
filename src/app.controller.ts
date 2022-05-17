88aa6160a138414b949d295f44651ae740e1950e
Visal Hettiarachchi <vish847255@gmail.com>
2022-05-17 13:06:37 +0530

    implement filter symptoms functionality

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @HttpCode(200)
+  async getAllSymptomsInWeb(): Promise<any> {
+    let response =  await this.appService.getAllSymptomes();
+    return { "response": response};
+  }
+
+  @Post('filter-symptoms')
+  @HttpCode(200)
+  async getAllSymptoms(@Body() messageBody: any): Promise<any> {
+    const textExtract = messageBody.content;
+
+    let symptomsList =  await this.appService.getAllSymptomes();
+    let res = this.appService.getFilteredSymptoms(symptomsList,textExtract);
+
+    return { "response": res};
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,52 @@
 import { Injectable } from '@nestjs/common';
+import { html } from 'cheerio/lib/api/manipulation';
+var rp = require('request-promise');
+const cheerio = require('cheerio');
+const url = 'https://www.nhsinform.scot/symptoms-and-self-help/a-to-z/';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+
+  getAllSymptomes(): Promise<any> {
+    return new Promise<any>((resolve, reject) => {
+      rp(url).then((htmlString: string) => {
+          let symptomsList = [];
+          const $ = cheerio.load(htmlString);
+
+          for (let i = 0; i < 26; i++) {
+            let letter = (i + 10).toString(36);
+
+            $(`#${letter}`).next().each((idx, ref) => {
+              const elem = $(ref);
+              let itemsList = [];
+
+              $(elem).find('li').each((idx, elem) => {
+                const key = $(elem).text().trim();
+                itemsList.push(key);
+              });
+
+              itemsList.pop(); // removes back to top element
+              symptomsList.push(...itemsList);
+              itemsList = [];
+
+              });
+          }
+
+          resolve(symptomsList);
+        })
+        .catch( (err) => {
+          reject(err);
+        });
+    });
+  }
+
+  getFilteredSymptoms(symptomsList : [], text: string) : any {
+    const filteredList = [];
+    symptomsList.forEach(symptom => {
+      if(text.includes(symptom)){
+        filteredList.push(symptom);
+      }
+    })
+    return filteredList;
   }
 }
